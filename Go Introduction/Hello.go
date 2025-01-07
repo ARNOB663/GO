8079a249bc7360ef5f6bf4669491a449dc494f9e
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-07 22:41:17 +0600

    Update Hello.go

--- Go Introduction/Hello.go
+++ Go Introduction/Hello.go
@@ -3,5 +3,5 @@ package main // Package main is special. It defines a standalone executable prog
 import "fmt" // Package fmt implements formatted I/O with functions analogous to C's printf and scanf. The format 'verbs' are derived from C's but are simpler.
 
 func main(){
- fmt.Println("Hello World")// Println formats using the default formats for its operands and writes to standard output. Spaces are always added between operands and a newline is appended. It returns the number of bytes written and any write error encountered.
+ fmt.Println("Hello, World")// Println formats using the default formats for its operands and writes to standard output. Spaces are always added between operands and a newline is appended. It returns the number of bytes written and any write error encountered.
 }
