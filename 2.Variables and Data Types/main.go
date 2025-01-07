dccee577bce0c81837fa018120ed0349844a056c
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-07 22:50:00 +0600

    Create main.go

---
+++ 2.Variables and Data Types/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main(){
+a:= 10; // Variable declaration and assignment
+// a is a variable of type int
+fmt.Println(a);
+/* Data Types in Go */
+// 1. Boolean
+var b bool = true;
+fmt.Println(b);
+// 2. Numeric
+var c int = 10;
+fmt.Println(c);
+var d float64 = 10.5;
+fmt.Println(d);
+// 3. String
+var e string = "Hello, World";
+fmt.Println(e);
+// 4. Derived
+// 4.1. Pointer
+var f *int = &a;
+fmt.Println(f);
+// 4.2. Array
+var g [5]int = [5]int{1,2,3,4,5};
+fmt.Println(g);
+
+}
