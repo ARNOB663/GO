f6d4d69519aae1e79806667eaa1d8d0022f81985
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-07 22:40:32 +0600

    Update Hello.go

--- Go Introduction/Hello.go
+++ Go Introduction/Hello.go
@@ -4,5 +4,4 @@ import "fmt" // Package fmt implements formatted I/O with functions analogous to
 
 func main(){
  fmt.Println("Hello World")// Println formats using the default formats for its operands and writes to standard output. Spaces are always added between operands and a newline is appended. It returns the number of bytes written and any write error encountered.
-
 }
